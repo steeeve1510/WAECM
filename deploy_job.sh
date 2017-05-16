b952710189693b8fa5ae2873b6f9cbdc1cdd0a34
Stefan Griesler <e1125257@student.tuwien.ac.at>
2017-05-16 17:19:30 +0200

    Added Deploy script

---
+++ deploy_job.sh
@@ -0,0 +1,5 @@
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+docker tag waecm-bsp1:latest 313230979214.dkr.ecr.us-west-2.amazonaws.com/waecm-bsp1:latest
+docker push 313230979214.dkr.ecr.us-west-2.amazonaws.com/waecm-bsp1:latest
+chmod 600 EC2.pem && ssh -o StrictHostKeyChecking=no -i EC2.pem ec2-user@ec2-54-187-122-131.us-west-2.compute.amazonaws.com ./deploy.sh
